521c81e57b86fedabc819c409baf7f5bd67759d0
Mauricio <mauricio2017rubiale@gmail.com>
2021-01-15 18:51:59 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -62,6 +62,10 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Watting player: " + chessMatch.getCurrentPlayer());
+        if (chessMatch.getCheck()) {
+            System.out.println("CHECK!");
+        }
+    
     }
 
     public static void printBoard(ChessPiece[][] Pieces){
